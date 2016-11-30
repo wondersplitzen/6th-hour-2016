7129c62d4394a3ae99cd5a92256653afe6e4bfe0
hongkaij <hongkaijiang@claytonschools.net>
2016-11-30 12:12:43 -0600

    dgrfdf

--- team3.py
+++ team3.py
@@ -26,7 +26,11 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if their_history=='':
+        return 'c'
+    if their_history[-1]=='b':
+        return 'b'
+
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
